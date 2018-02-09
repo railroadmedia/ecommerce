7e6519c2d58952bd6ea45f76d6ae35c0deb4fa34
roxanariza <roxana_r2000@yahoo.com>
2018-02-09 16:02:48 +0200

    First test

---
+++ tests/Functional/OrderTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Railroad\Ecommerce\Tests\Functional;
+
+use PHPUnit\Framework\TestCase;
+use Railroad\Ecommerce\Tests\EcommerceTestCase;
+
+class OrderTest extends EcommerceTestCase
+{
+    protected function setUp()
+    {
+        parent::setUp();
+    }
+
+    public function test_first()
+    {
+        $this->assertTrue(true);
+    }
+}
