cb8424426306c24d172c12e31be0c5fd7e76e042
calebfavor <calebfavor@gmail.com>
2019-04-16 15:52:16 -0700

    Order claiming service test WIP, basic setup is ready.

--- tests/Functional/Services/OrderClaimingServiceTest.php
+++ tests/Functional/Services/OrderClaimingServiceTest.php
@@ -2,12 +2,15 @@
 
 namespace Railroad\Ecommerce\Tests\Functional\Services;
 
-use Illuminate\Auth\AuthManager;
-use Illuminate\Contracts\Auth\Factory;
 use PHPUnit\Framework\MockObject\MockObject;
+use Railroad\Ecommerce\Entities\Address;
+use Railroad\Ecommerce\Entities\Payment;
+use Railroad\Ecommerce\Entities\PaymentMethod;
+use Railroad\Ecommerce\Entities\Structures\Cart;
 use Railroad\Ecommerce\Entities\Structures\Purchaser;
+use Railroad\Ecommerce\Services\CartService;
 use Railroad\Ecommerce\Services\OrderClaimingService;
-use Railroad\Ecommerce\Services\PurchaserService;
+use Railroad\Ecommerce\Services\ShippingService;
 use Railroad\Ecommerce\Tests\EcommerceTestCase;
 
 class OrderClaimingServiceTest extends EcommerceTestCase
@@ -17,17 +20,67 @@ class OrderClaimingServiceTest extends EcommerceTestCase
      */
     protected $orderClaimingService;
 
+    /**
+     * @var MockObject
+     */
+    protected $cartServiceMock;
+
+    /**
+     * @var MockObject
+     */
+    protected $shippingServiceMock;
+
     protected function setUp()
     {
         parent::setUp();
 
         // mocks
+        $this->cartServiceMock =
+            $this->getMockBuilder(CartService::class)
+                ->disableOriginalConstructor()
+                ->getMock();
+
+        $this->shippingServiceMock =
+            $this->getMockBuilder(ShippingService::class)
+                ->disableOriginalConstructor()
+                ->getMock();
+
+        $this->app->instance(CartService::class, $this->cartServiceMock);
+        $this->app->instance(ShippingService::class, $this->shippingServiceMock);
 
         $this->orderClaimingService = app()->make(OrderClaimingService::class);
     }
 
     public function test_claim_order()
     {
-        // WIP
+        $dueForOrder = rand();
+        $totalItemCosts = rand();
+        $totalFinanceCosts = rand();
+        $taxDueForOrder = rand();
+        $shippingDueForOrder = rand();
+
+        $this->cartServiceMock->method('getDueForOrder')
+            ->willReturn($dueForOrder);
+        $this->cartServiceMock->method('getTotalItemCosts')
+            ->willReturn($totalItemCosts);
+        $this->cartServiceMock->method('getTotalFinanceCosts')
+            ->willReturn($totalFinanceCosts);
+        $this->cartServiceMock->method('getTaxDueForOrder')
+            ->willReturn($taxDueForOrder);
+        $this->shippingServiceMock->method('getShippingDueForCart')
+            ->willReturn($shippingDueForOrder);
+
+        // todo: populate test entities before passing to service being tested
+        $purchaser = new Purchaser();
+        $payment = new Payment();
+        $cart = new Cart();
+
+        $paymentMethod = new PaymentMethod();
+        $billingAddress = new Address();
+
+        $paymentMethod->setBillingAddress($billingAddress);
+        $payment->setPaymentMethod($paymentMethod);
+
+        $order = $this->orderClaimingService->claimOrder($purchaser, $payment, $cart);
     }
 }
