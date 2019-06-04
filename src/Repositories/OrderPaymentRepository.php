ffe4a4693257ad60f1cc41c5007914ba19c698ed
calebfavor <calebfavor@gmail.com>
2019-06-04 10:05:14 -0700

    Pulling stats fix from 2.1

--- src/Repositories/OrderPaymentRepository.php
+++ src/Repositories/OrderPaymentRepository.php
@@ -2,10 +2,7 @@
 
 namespace Railroad\Ecommerce\Repositories;
 
-use Carbon\Carbon;
 use Doctrine\ORM\QueryBuilder;
-use Illuminate\Http\Request;
-use Railroad\Ecommerce\Entities\Order;
 use Railroad\Ecommerce\Entities\OrderPayment;
 use Railroad\Ecommerce\Entities\Payment;
 use Railroad\Ecommerce\Managers\EcommerceEntityManager;
@@ -58,56 +55,5 @@ class OrderPaymentRepository extends RepositoryBase
             ->setParameter('payment', $payment);
 
         return $qb->getQuery()->getResult();
-    }
-
-    /**
-     * @param Request $request
-     *
-     * @return Order[]
-     */
-    public function getOrderPaymentsForStats(Request $request): array
-    {
-        $smallDateTime =
-            $request->get(
-                'small_date_time',
-                Carbon::now()
-                    ->subDay()
-                    ->toDateTimeString()
-            );
-
-        $bigDateTime =
-            $request->get(
-                'big_date_time',
-                Carbon::now()
-                    ->subDay()
-                    ->toDateTimeString()
-            );
-
-        /** @var $qb QueryBuilder */
-        $qb =
-            $this->getEntityManager()
-                ->createQueryBuilder();
-
-        $qb->select(['op', 'o', 'oi', 'p', 'py'])
-            ->from(OrderPayment::class, 'op')
-            ->join('op.order', 'o')
-            ->join('op.payment', 'py')
-            ->join('o.orderItems', 'oi')
-            ->join('oi.product', 'p')
-            ->where(
-                $qb->expr()
-                    ->between('py.createdAt', ':smallDateTime', ':bigDateTime')
-            )
-            ->setParameter('smallDateTime', $smallDateTime)
-            ->setParameter('bigDateTime', $bigDateTime);
-
-        if ($request->has('brand')) {
-            $qb->andWhere('o.brand = :brand')
-                ->setParameter('brand', $request->get('brand'));
-        }
-
-        return
-            $qb->getQuery()
-                ->getResult();
     }
 }
