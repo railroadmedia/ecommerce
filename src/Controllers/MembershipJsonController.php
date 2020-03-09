bd87d0b651d8b191d6fb460e4468595d702e5d51
Bogdan Damian <bogdan.damian@artsoft-consult.ro>
2020-03-09 17:14:26 +0200

    added brand filter and csv download option to membership statistics API - PMM-353 & PMM-355

--- src/Controllers/MembershipJsonController.php
+++ src/Controllers/MembershipJsonController.php
@@ -70,10 +70,54 @@ class MembershipJsonController extends Controller
                 ->endOfDay();
 
         $intervalType = $request->get('interval_type');
+        $brand = $request->get('brand');
 
-        $stats = $this->membershipStatsRepository->getStats($smallDateTime, $bigDateTime, $intervalType);
+        $stats = $this->membershipStatsRepository->getStats($smallDateTime, $bigDateTime, $intervalType, $brand);
 
-        // dd($stats);
+        if ($request->has('csv') && $request->get('csv') == true) {
+            $rows = [];
+
+            foreach ($stats as $membershipStats) {
+                $rows[] = [
+                    $membershipStats->getBrand(),
+                    $membershipStats->getStatsDate(),
+                    $membershipStats->getNew(),
+                    $membershipStats->getActiveState(),
+                    $membershipStats->getExpired(),
+                    $membershipStats->getSuspendedState(),
+                    $membershipStats->getCanceled(),
+                    $membershipStats->getCanceledState(),
+                    $membershipStats->getIntervalType(),
+                ];
+            }
+
+            $filePath = sys_get_temp_dir() . "/membership-stats-" . time() . ".csv";
+
+            $f = fopen($filePath, "w");
+
+            fputcsv(
+                $f,
+                [
+                    'Membership Brand',
+                    'Stats Date',
+                    'New',
+                    'Active State',
+                    'Expired',
+                    'Suspended State',
+                    'Canceled',
+                    'Canceled State',
+                    'Interval Type',
+                ]
+            );
+
+            foreach ($rows as $line) {
+                fputcsv($f, $line);
+            }
+
+            return response()
+                ->download($filePath)
+                ->deleteFileAfterSend();
+        }
 
         return ResponseService::membershipStats($stats)
             ->respond(200);
