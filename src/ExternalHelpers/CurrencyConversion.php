5bb8d5063927163ea4567bfd99e8f19d7a831f70
Roxana Riza <roxana_r2000@yahoo.com>
2021-02-12 18:28:00 +0200

    Convert user local price to USD method

---
+++ src/ExternalHelpers/CurrencyConversion.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Railroad\Ecommerce\ExternalHelpers;
+
+class CurrencyConversion
+{
+    /**
+     * @param $value
+     * @param $from
+     * @param string $to
+     * @return float|int
+     */
+    public function convert($value, $from, $to = 'USD')
+    {
+        $client = new \GuzzleHttp\Client();
+
+        $url = 'https://api.exchangeratesapi.io/latest?symbols=' . $to . '&base=' . $from;
+
+        $exchangeRate = json_decode(
+            $client->get($url)
+                ->getBody()
+                ->getContents(),
+            true
+        )['rates'][$to];
+
+        $amountUSD = (float)$exchangeRate * $value;
+
+        return $amountUSD;
+    }
+}
