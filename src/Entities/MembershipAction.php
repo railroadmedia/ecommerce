045d4787fa722e9ce6dd0ef81be4bb77b61b6a90
Jonathan Maingot <jonathan@drumeo.com>
2022-03-17 10:07:47 -0700

    Add more constants to MembershipAction
    for use with UserAccessService classes methods used by railroad/crux package

--- src/Entities/MembershipAction.php
+++ src/Entities/MembershipAction.php
@@ -80,6 +80,9 @@ class MembershipAction
     public const ACTION_EXTEND_FOR_AMOUNT_OF_DAYS = 'extended for amount of days';
     public const ACTION_EXTEND_FOR_AMOUNT_OF_DAYS_GRATIS_FOR_RETENTION =
         'extended for amount of days gratis for retention offer';
+    public const ACTION_EXTEND_FOR_AMOUNT_OF_MONTHS = 'extended for amount of months';
+    public const ACTION_EXTEND_FOR_AMOUNT_OF_MONTH_GRATIS_FOR_RETENTION =
+        'extended for amount of months gratis for retention offer';
     public const ACTION_SWITCH_TO_NEW_PRICE = 'switched to new price';
     public const ACTION_SWITCH_TO_NEW_PRICE_IN_CENTS = 'switched to new price in cents';
     public const ACTION_SWITCH_BILLING_INTERVAL_TO_MONTHLY = 'switched billing interval to monthly';
@@ -87,6 +90,8 @@ class MembershipAction
     public const ACTION_REQUESTED_HELP = 'requested help';
     public const ACTION_REQUESTED_TECHNICAL_SUPPORT = 'requested technical support';
     public const ACTION_CANCELLED = 'cancelled';
+    public const ACTION_CANCELLATION_WINBACK = 'accepted cancellation winback offer';
+    public const ACTION_CANCELLATION_RETENTION = 'accepted retention offer';
 
     /**
      * Address constructor.
