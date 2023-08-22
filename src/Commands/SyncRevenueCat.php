2b4eb335354020fa7e359533379bfafbcfb72e4e
Roxana Riza <roxana_r2000@yahoo.com>
2023-08-22 15:30:45 +0300

    Update SyncRevenueCat - Artisan command used for testing

--- src/Commands/SyncRevenueCat.php
+++ src/Commands/SyncRevenueCat.php
@@ -2,11 +2,15 @@
 
 namespace Railroad\Ecommerce\Commands;
 
+
 use Exception;
 use Illuminate\Console\Command;
 use Illuminate\Database\DatabaseManager;
+use Illuminate\Support\Collection;
 use Railroad\Ecommerce\Contracts\UserProviderInterface;
+use Railroad\Ecommerce\Entities\AppleReceipt;
 use Railroad\Ecommerce\Gateways\RevenueCatGateway;
+use Railroad\Ecommerce\Gateways\AppleStoreKitGateway;
 use ReceiptValidator\GooglePlay\SubscriptionResponse;
 use ReceiptValidator\iTunes\ProductionResponse;
 use ReceiptValidator\iTunes\SandboxResponse;
@@ -19,7 +23,7 @@ class SyncRevenueCat extends Command
      *
      * @var string
      */
-    protected $name = 'SyncRevenueCat';
+    protected $signature = 'SyncRevenueCat {id}';
 
     /**
      * The console command description.
@@ -36,6 +40,7 @@ class SyncRevenueCat extends Command
     public function handle(
         DatabaseManager $databaseManager,
         RevenueCatGateway $revenueCatGateway,
+        AppleStoreKitGateway $appleStoreGateway,
         UserProviderInterface $userProvider
     ) {
         ini_set('xdebug.var_display_max_depth', '100');
@@ -44,50 +49,152 @@ class SyncRevenueCat extends Command
 
         $this->info('Starting SyncRevenueCat command.');
 
-        $googleReceipts =
-            $databaseManager->connection(config('ecommerce.database_connection_name'))
-                ->table('ecommerce_google_receipts')
-                ->get();
-
-        foreach ($googleReceipts as $googleReceipt) {
-            $user = $userProvider->getUserByEmail($googleReceipt->email);
-            if ($user) {
-                $revenueCatGateway->sendRequest(
-                    $googleReceipt->purchase_token,
-                    $user,
-                    $googleReceipt->product_id,
-                    'android'
-                );
-            }
-        }
-
-        $this->info('Google done.');
+//        $googleReceipts =
+//            $databaseManager->connection(config('ecommerce.database_connection_name'))
+//                ->table('ecommerce_google_receipts')
+//                ->where('ecommerce_google_receipts.id', '=', $this->argument('id'))
+//                ->get();
+//
+//        foreach ($googleReceipts as $googleReceipt) {
+//            $user = $userProvider->getUserByEmail($googleReceipt->email);
+//
+//            if ($user) {
+//                $revenueCatGateway->sendRequest(
+//                    $googleReceipt->purchase_token,
+//                    $user,
+//                    $googleReceipt->product_id,
+//                    'android'
+//                );
+//            }
+//        }
+//
+//        $this->info('Google done.');
+//        return;
 
         $appleReceipts =
             $databaseManager->connection(config('ecommerce.database_connection_name'))
                 ->table('ecommerce_apple_receipts')
-                ->orderBy('id', 'desc')
-                ->get();
+                ->join('ecommerce_subscriptions', 'ecommerce_apple_receipts.subscription_id', '=', 'ecommerce_subscriptions.id')
+                ->orderBy('ecommerce_apple_receipts.id', 'desc')
+//                ->whereNull('product_id')
+//                ->whereNotNull('subscription_id')
+                //->where('request_type', '=', 'mobile')
+                ->where('ecommerce_apple_receipts.id', '=', $this->argument('id'))
+                ->chunk(
+                    500,
+                    function (Collection $appleReceipts) use ( $databaseManager, $appleStoreGateway, $userProvider, $revenueCatGateway) {
 
-        foreach ($appleReceipts as $appleReceipt) {
-            $notificationRequestData = unserialize(base64_decode($appleReceipt->raw_receipt_response, true));
-            if ($notificationRequestData !== false) {
-                $latestReceiptInfo = $notificationRequestData->getLatestReceiptInfo();
-                if (!empty($latestReceiptInfo)) {
-                    $latestPurchasedInfo = $latestReceiptInfo[0];
+                        foreach ($appleReceipts as $appleReceipt) {
+                            $this->info('Processing apple receipt: ' . $appleReceipt->id);
+                            try {
+                                $notificationRequestData =
+                                    unserialize(base64_decode($appleReceipt->raw_receipt_response, true));
 
-                    $user = $userProvider->getUserByEmail($appleReceipt->email);
-                    if ($user) {
-                        $revenueCatGateway->sendRequest(
-                            $appleReceipt->receipt,
-                            $user,
-                            $latestPurchasedInfo->getProductId(),
-                            'ios'
-                        );
+                                if ($notificationRequestData !== false && $notificationRequestData !== null) {
+                                    $latestReceiptInfo = $notificationRequestData->getLatestReceiptInfo();
+                                    if (!empty($latestReceiptInfo) ) {
+
+                                        $user = $userProvider->getUserById($appleReceipt->user_id);
+
+                                                                if ($user) {
+                                                                    $revenueCatGateway->sendRequest(
+                                                                        $appleReceipt->receipt,
+                                                                        $user,
+                                                                        $latestReceiptInfo[0]->getProductId(),
+                                                                        'ios',
+                                                                        $appleReceipt->local_price,
+                                                                        $appleReceipt->local_currency,
+                                                                    );
+                                                                }
+                                    }else{
+                                        $this->error('raw_receipt_response exists, but No latestReceiptInfo for apple receipt: ' . $appleReceipt->id);
+                                        $response = $appleStoreGateway->getResponse($appleReceipt->receipt);
+
+                                        $latestReceiptInfo = $response->getLatestReceiptInfo();
+                                        if (!empty($latestReceiptInfo)) {
+                                            $user = $userProvider->getUserByEmail($appleReceipt->email);
+
+                                                                    if ($user) {
+                                                                        $revenueCatGateway->sendRequest(
+                                                                            $appleReceipt->receipt,
+                                                                            $user,
+                                                                            $latestReceiptInfo[0]->getProductId(),
+                                                                            'ios',
+                                                                            $appleReceipt->local_price,
+                                                                            $appleReceipt->local_currency,
+                                                                        );
+                                                                    }
+                                        }else{
+                                            $this->error('No latestReceiptInfo for apple receipt: ' . $appleReceipt->id);
+                                        }
+                                    }
+                                    //                    if (!empty($latestReceiptInfo)) {
+                                    //                        $latestPurchasedInfo = $latestReceiptInfo[0];
+                                    //                        dd($appleReceipt);
+                                    //dd( $latestPurchasedInfo->getProductId());
+                                    //                        $user = $userProvider->getUserByEmail($appleReceipt->email);
+                                    //
+                                    //                        if ($user) {
+                                    //                            $revenueCatGateway->sendRequest(
+                                    //                                $appleReceipt->receipt,
+                                    //                                $user,
+                                    //                                $latestPurchasedInfo->getProductId(),
+                                    //                                'ios',
+                                    //                                $appleReceipt->local_price,
+                                    //                                $appleReceipt->local_currency,
+                                    //                            );
+                                    //                        }
+                                    //                    }
+                                }else{
+                                    $response = $appleStoreGateway->getResponse($appleReceipt->receipt);
+                                    $latestReceiptInfo = $response->getLatestReceiptInfo();
+                                    if (!empty($latestReceiptInfo)) {
+                                        $user = $userProvider->getUserByEmail($appleReceipt->email);
+
+                                                                if ($user) {
+                                                                    $revenueCatGateway->sendRequest(
+                                                                        $appleReceipt->receipt,
+                                                                        $user,
+                                                                        $latestReceiptInfo[0]->getProductId(),
+                                                                        'ios',
+                                                                        $appleReceipt->local_price,
+                                                                        $appleReceipt->local_currency,
+                                                                    );
+                                                                }
+                                    }else{
+                                        $this->error('No latestReceiptInfo for apple receipt: ' . $appleReceipt->id);
+                                    }
+                                }
+                            } catch (Exception $exception) {
+                                $this->info('Exception for apple receipt: ' . $appleReceipt->id);
+                               // dd($appleStoreGateway);
+                                $response = $appleStoreGateway->getResponse($appleReceipt->receipt);
+                                $latestReceiptInfo = $response->getLatestReceiptInfo();
+
+                                if (!empty($latestReceiptInfo)) {
+                                    $user = $userProvider->getUserByEmail($appleReceipt->email);
+
+                                                            if ($user) {
+                                                                $revenueCatGateway->sendRequest(
+                                                                    $appleReceipt->receipt,
+                                                                    $user,
+                                                                    $latestReceiptInfo[0]->getProductId(),
+                                                                    'ios',
+                                                                    $appleReceipt->local_price,
+                                                                    $appleReceipt->local_currency,
+                                                                );
+                                                            }
+                                }else{
+                                  //  dd($response);
+                                    $this->error('No latestReceiptInfo for apple receipt: ' . $appleReceipt->id);
+                                }
+                               // dd($response->getLatestReceiptInfo());
+                                //continue;
+                                //dd($exception);
+                            }
+                        }
                     }
-                }
-            }
-        }
+            );
 
         $this->info('Finished SyncRevenueCat.');
     }
