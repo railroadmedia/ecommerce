c5ee6d6c8753c9cb3fc1a5713c53d1233f0518dd
Roxana Riza <roxana_r2000@yahoo.com>
2023-10-02 13:45:00 +0300

    Update Revenuecat puchase and update subscriber attributtes methods

--- src/Gateways/RevenueCatGateway.php
+++ src/Gateways/RevenueCatGateway.php
@@ -3,13 +3,19 @@
 namespace Railroad\Ecommerce\Gateways;
 
 use GuzzleHttp\Exception\GuzzleException;
-use Railroad\Ecommerce\Exceptions\ReceiptValidationException;
-use ReceiptValidator\iTunes\ResponseInterface;
-use ReceiptValidator\iTunes\Validator;
-use Throwable;
 
 class RevenueCatGateway
 {
+    /**
+     * @param $receipt
+     * @param $user
+     * @param $productId
+     * @param $platform
+     * @param null $localPrice
+     * @param null $currency
+     * @param string $app
+     * @return string
+     */
     public function sendRequest(
         $receipt,
         $user,
@@ -37,6 +43,106 @@ class RevenueCatGateway
 
         try {
             $response = $client->request('POST', 'https://api.revenuecat.com/v1/receipts', [
+                'body' => json_encode($bod),
+                'headers' => [
+                    'X-Platform' => $platform,
+                    'accept' => 'application/json',
+                    'content-type' => 'application/json',
+                    'Authorization' => 'Bearer '.config('ecommerce.revenuecat.'.$platform)[$app],
+                ],
+            ]);
+        } catch (GuzzleException $exception) {
+            error_log($exception->getMessage());
+
+            return $exception->getMessage();
+        }
+
+        return $response->getBody()
+            ->getContents();
+    }
+
+    /**
+     * @param $receipt
+     * @param null $productId
+     * @param $platform
+     * @param null $localPrice
+     * @param null $currency
+     * @param string $app
+     * @param null $userEmail
+     * @param null $userId
+     * @return string
+     */
+    public function purchase(
+        $receipt,
+        $productId = null,
+        $platform,
+        $localPrice = null,
+        $currency = null,
+        $app = 'Musora',
+        $userEmail = null,
+        $userId = null
+    ) {
+        $client = new \GuzzleHttp\Client();
+
+        $bod = [
+            'product_id' => $productId,
+            'fetch_token' => $receipt,
+            'price' => $localPrice,
+            'currency' => $currency,
+            'attributes' => [
+                'email' => [
+                    'value' => $userEmail,
+                ],
+            ],
+        ];
+        if ($userId) {
+            $bod['app_user_id'] = "$userId";
+        }
+
+        try {
+           // dd(config('ecommerce.revenuecat.'.$platform)[$app]);
+            $response = $client->request('POST', 'https://api.revenuecat.com/v1/receipts', [
+                'body' => json_encode($bod),
+                'headers' => [
+                    'X-Platform' => $platform,
+                    'accept' => 'application/json',
+                    'content-type' => 'application/json',
+                    'Authorization' => 'Bearer '.config('ecommerce.revenuecat.'.$platform)[$app],
+                ],
+            ]);
+        } catch (GuzzleException $exception) {
+            error_log($exception->getMessage());
+
+            return $exception->getMessage();
+        }
+
+        return $response->getBody()
+            ->getContents();
+    }
+
+    /**
+     * @param $userId
+     * @param $attributes
+     * @param $musoraUserId
+     * @param $platform
+     * @param string $app
+     * @return string
+     */
+    public function updateSubscriberAttribute($userId, $attributes, $musoraUserId, $platform, $app = 'Musora')
+    {
+        $client = new \GuzzleHttp\Client();
+        $att = [];
+        foreach ($attributes as $key => $value) {
+            $att[$key] = [
+                'value' => $value,
+            ];
+        }
+        $bod = [
+            'attributes' => $att,
+        ];
+
+        try {
+            $response = $client->request('POST', 'https://api.revenuecat.com/v1/subscribers/'.$userId.'/attributes', [
                 'body' => json_encode($bod),
                 'headers' => [
                     'X-Platform' => $platform,
